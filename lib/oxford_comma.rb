8b77a4bb99ef5780d35f23853aae89fd0d5d7e25
Mo Al <malbazaz@mba2018.hbs.edu>
2018-06-13 02:34:11 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
-def oxford_comma(array)
-
+def oxford_comma(array, x=" and ",y=", ")
+  if array.length<3
+small_string= array.join(x)
+return small_string
+  elsif array.length>2
+  y=", "
+newnumb=array.length
+  newnumb-=1 
+  lastword = array[newnumb]
+array.pop
+ longstring =array.join(y)
+newlastword = ", and #{lastword}"  
+  longstring << newlastword
+  return longstring
+end
 end
